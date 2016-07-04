215b87b3ab6fd51f6e60c6518bc81bec2873a28b
Drew Conly <atconly@gmail.com>
2016-07-04 09:26:57 -0600

    finish string-reporter

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Happy Fourth!");
+var word = prompt("Please Enter a word");
+
+if (word != null) {
+  document.getElementById("word-name").innerHTML =
+  word;
+  document.getElementById("word-length").innerHTML =
+  "There are " + word.length + " characters in this word";
+  document.getElementById("third-letter").innerHTML =
+  "The third letter is '" + word.charAt(2) + "'";
+  document.getElementById("lowercase").innerHTML =
+  "Lowercase: " + word.toLowerCase();
+  document.getElementById("uppercase").innerHTML =
+  "Uppercase: " + word.toUpperCase();
+  document.getElementById("example").innerHTML =
+  "I like the word '" + word + "'";
+  document.getElementById("subword").innerHTML =
+  "Uppercase: " + word.substring(1, 4);
+}
